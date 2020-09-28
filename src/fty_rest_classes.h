e3e0078e3abafadf95a83ebb064c7e448dd02edb
Jean-Baptiste Boric <Jean-BaptisteBORIC@Eaton.com>
2020-09-28 11:15:51 +0200

    Add actions by asset REST endpoint

--- src/fty_rest_classes.h
+++ src/fty_rest_classes.h
@@ -117,6 +117,10 @@ typedef struct _web_src_alert_templates_t web_src_alert_templates_t;
 typedef struct _web_src_asset_delete_t web_src_asset_delete_t;
 #define WEB_SRC_ASSET_DELETE_T_DEFINED
 #endif
+#ifndef WEB_SRC_ACTIONS_GET_T_DEFINED
+typedef struct _web_src_actions_get_t web_src_actions_get_t;
+#define WEB_SRC_ACTIONS_GET_T_DEFINED
+#endif
 #ifndef WEB_SRC_ASSET_ACTIONS_GET_T_DEFINED
 typedef struct _web_src_asset_actions_get_t web_src_asset_actions_get_t;
 #define WEB_SRC_ASSET_ACTIONS_GET_T_DEFINED
@@ -343,6 +347,7 @@ typedef struct _web_src_hw_capability_t web_src_hw_capability_t;
 #include "web/src/alert_rules_list.h"
 #include "web/src/alert_templates.h"
 #include "web/src/asset_DELETE.h"
+#include "web/src/actions_GET.h"
 #include "web/src/asset_actions_GET.h"
 #include "web/src/asset_actions_POST.h"
 #include "web/src/asset_export.h"
